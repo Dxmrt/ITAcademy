1a567a2905c3709896aad7072c0bfacf86cb03d0
Sonia Vera <douxmort@gmail.com>
2024-04-29 13:04:12 +0200

    primer commit

---
+++ src/main/java/TascaS1_01/N1/n1exercici1/Percussion.java
@@ -0,0 +1,10 @@
+package TascaS1_01.N1.n1exercici1;
+
+public class Percussion extends Instrument{
+    public Percussion(String nom, float preu){
+        super(nom, preu);
+    }
+    public String play(){
+        return "A percussion it's being played";
+    }
+}
