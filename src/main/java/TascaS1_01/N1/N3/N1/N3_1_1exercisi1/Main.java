ff84a261d322389e763393ff4ecfad31c3d7fbd6
Sonia Vera <douxmort@gmail.com>
2024-05-21 12:03:12 +0200

    N31

---
+++ src/main/java/TascaS1_01/N1/N3/N1/N3_1_1exercisi1/Main.java
@@ -0,0 +1,43 @@
+package TascaS1_01.N1.N3.N1.N3_1_1exercisi1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Month> monthList = new ArrayList<>();
+        monthList.add(new Month("Gener"));
+        monthList.add(new Month("Febrer"));
+        monthList.add(new Month("Març"));
+        monthList.add(new Month("Abril"));
+        monthList.add(new Month("Maig"));
+        monthList.add(new Month("Juny"));
+        monthList.add(new Month("Juliol"));
+        monthList.add(new Month("Setembre"));
+        monthList.add(new Month("Octubre"));
+        monthList.add(new Month("Novembre"));
+        monthList.add(new Month("Desembre"));
+
+        System.out.println("Llista de mesos: " + monthList);
+
+        monthList.add(7, new Month("Agost")); //Afegim Agost a la posicio 7
+        System.out.println("Llista de mesos: " + monthList);
+
+        HashSet<Month> monthsSet = new HashSet<>(monthList); //Converteix ArrayList en un HashSet
+        System.out.println(monthsSet);
+
+        System.out.println("\nRecorre amb for");
+
+        for (Month month : monthList) {
+            System.out.println(month);
+        }
+        System.out.println("\nRecorre la llista amb un iterador:");
+        Iterator<Month> iterator = monthList.iterator();
+        if (iterator.hasNext()) {
+            do {
+                System.out.println(iterator.next());
+            } while (iterator.hasNext());
+        }
+
+
+    }
+}
---
+++ src/main/java/TascaS1_01/N1/N3/N1/N3_1_1exercisi1/Month.java
@@ -0,0 +1,20 @@
+package TascaS1_01.N1.N3.N1.N3_1_1exercisi1;
+
+public class Month {
+    public String nom;
+
+    public Month(String nom){
+        this.nom = nom;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public String toString(){
+        return nom;
+    }
+}
+
+
+
