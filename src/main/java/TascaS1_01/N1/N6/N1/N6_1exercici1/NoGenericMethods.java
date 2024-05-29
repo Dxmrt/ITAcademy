aba5bfea8f43185ad86addf30b7d29c9efe20f5a
Sonia Vera <douxmort@gmail.com>
2024-05-29 10:40:46 +0200

    N6_1

--- src/main/java/TascaS1_01/N1/N6/N1/N6_1exercici1/NoGenericMethods.java
+++ src/main/java/TascaS1_01/N1/N6/N1/N6_1exercici1/NoGenericMethods.java
@@ -2,7 +2,7 @@ package TascaS1_01.N1.N6.N1.N6_1exercici1;
 
 public class NoGenericMethods {
 
-    private Object[] objects;
+    private final Object[] objects;
 
     public NoGenericMethods(String obj1, String obj2, String obj3) {
         objects = new Object[]{obj1, obj2, obj3};
