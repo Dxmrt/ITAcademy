68b9073f48f4722584b6bfddb69585db9226441c
Sonia Vera <douxmort@gmail.com>
2024-05-29 23:58:36 +0200

    N7_3

--- src/main/java/TascaS1_01/N1/N7/N1/N7_1exercici2/TreballadorOnline.java
+++ src/main/java/TascaS1_01/N1/N7/N1/N7_1exercici2/TreballadorOnline.java
@@ -3,7 +3,7 @@ package TascaS1_01.N1.N7.N1.N7_1exercici2;
 import TascaS1_01.N1.N7.N1.N7_1exercici1_2.Treballador;
 
 public class TreballadorOnline extends Treballador {
-    private static final double PREU_INTERNET = 30.0;
+
 
     public TreballadorOnline(String nom, String cognom, double preuHora) {
         super(nom, cognom, preuHora);
--- src/main/java/TascaS1_01/N1/N7/N1/N7_1exercici2/TreballadorPresencial.java
+++ src/main/java/TascaS1_01/N1/N7/N1/N7_1exercici2/TreballadorPresencial.java
@@ -3,7 +3,7 @@ package TascaS1_01.N1.N7.N1.N7_1exercici2;
 import TascaS1_01.N1.N7.N1.N7_1exercici1_2.Treballador;
 
 public class TreballadorPresencial extends Treballador {
-    private static final double BENZINA = 50.0;
+
 
     public TreballadorPresencial(String nom, String cognom, double preuHora) {
         super(nom, cognom, preuHora);
